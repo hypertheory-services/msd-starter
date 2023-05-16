351ab0d8025933eb86189698ae6ab111359749bd
Jeff Gonzalez <Jeff@hypertheory.com>
2023-05-16 11:41:45 -0400

    Initial

---
+++ DomainEvents/Jobs.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DomainEvents.Jobs;
+
+public record JobCreated
+{
+    public static string TOPIC = "jobs.created";
+    public Guid Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+}
